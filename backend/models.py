b4fe30bc8c6ba18e23352fa744b32d66f12f3398
lata-01 <64147396+lata-01@users.noreply.github.com>
2026-02-11 11:07:05 +0530

    Create models.py

---
+++ backend/models.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Column, Integer, String, Float
+from database import Base
+
+class Medicine(Base):
+    __tablename__ = "medicines"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    quantity = Column(Integer)
+    price = Column(Float)
